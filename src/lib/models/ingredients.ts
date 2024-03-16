0f28fbb95a4b2af67af8a07f345e9558442a90bb
sofiialives <127687828+sofiialives@users.noreply.github.com>
2024-03-16 16:51:48 +0100

    models' types

--- src/lib/models/ingredients.ts
+++ src/lib/models/ingredients.ts
@@ -1,6 +1,14 @@
 import mongoose, { Schema } from 'mongoose';
 
-const ingredientSchema = new Schema({});
+const ingredientSchema = new Schema({
+  localizedTitle: {
+    type: Array,
+    required: true,
+  },
+  image: {
+    type: String,
+  },
+});
 
 export const Ingredient =
   mongoose.models?.Ingredient || mongoose.model('Ingredient', ingredientSchema);
--- src/lib/models/news.ts
+++ src/lib/models/news.ts
@@ -1,5 +1,19 @@
 import mongoose, { Schema } from 'mongoose';
 
-const newsSchema = new Schema({});
+const newsSchema = new Schema({
+  localizedTitle: {
+    type: Array,
+    required: true,
+  },
+  image: {
+    type: Array,
+  },
+  description: {
+    type: Array,
+  },
+  date:{
+    type: Date
+  }
+});
 
 export const News = mongoose.models?.News || mongoose.model('news', newsSchema);

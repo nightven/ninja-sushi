2d008f3571363cee2ef3a51bf10e16ae6ab3ac5e
Oksana-Tnt <oksanatantsiura@gmail.com>
2024-03-19 19:12:17 +0100

    Update Category.tsx

--- src/components/Category/Category.tsx
+++ src/components/Category/Category.tsx
@@ -19,57 +19,57 @@ const Category = ({ updateCategory }: CategoryProps) => {
 
   return (
     <div className="container ">
-      <ul className="font-inter text-base font-medium px-4 py-2 mt-4 bg-bg mx-auto carousel carousel-center rounded-box space-x-2 w-[320px] tablet:w-[768px] desktop:flex desktop:w-[800px] desktop:mx-auto desktop:space-x-0 desktop:rounded-none">
+      <ul className="font-inter text-base font-medium px-4 py-2 mt-4 bg-bg mx-auto carousel carousel-start w-full rounded-box space-x-2 desktop:flex desktop:w-[800px] desktop:mx-auto desktop:space-x-0">
         <li
-          className="flex flex-col items-center py-1.5 px-6 carousel-item bg-white rounded-box desktop:rounded-none"
+          className="py-1.5 px-6 carousel-item bg-white rounded-box desktop:rounded-none desktop:rounded-l-2xl"
           onClick={changeCategory}
         >
-          <button className=" focus:stroke-orange-600 focus:text-orange-600 hover:text-orange-600 hover:stroke-orange-600 ">
+          <button className="flex flex-col items-center focus:stroke-orange-600 focus:text-orange-600 hover:text-orange-600 hover:stroke-orange-600 ">
             <Roll />
             <span>Роллы</span>
           </button>
         </li>
         <li
-          className="flex flex-col items-center py-1.5 px-6 carousel-item  bg-white rounded-box desktop:rounded-none"
+          className="py-1.5 px-6 carousel-item  bg-white rounded-box desktop:rounded-none"
           onClick={changeCategory}
         >
-          <button className=" focus:stroke-orange-600 focus:text-orange-600 hover:text-orange-600 hover:stroke-orange-600">
+          <button className="flex flex-col items-center focus:stroke-orange-600 focus:text-orange-600 hover:text-orange-600 hover:stroke-orange-600">
             <Sushi />
             <span>Суши</span>
           </button>
         </li>
         <li
-          className="flex flex-col items-center py-1.5 px-6 carousel-item  bg-white rounded-box desktop:rounded-none"
+          className="py-1.5 px-6 carousel-item  bg-white rounded-box desktop:rounded-none"
           onClick={changeCategory}
         >
-          <button className=" focus:stroke-orange-600 focus:text-orange-600 hover:text-orange-600 hover:stroke-orange-600">
+          <button className="flex flex-col items-center focus:stroke-orange-600 focus:text-orange-600 hover:text-orange-600 hover:stroke-orange-600">
             <Set />
             <span>Сеты</span>
           </button>
         </li>
         <li
-          className="flex flex-col items-center py-1.5 px-6 carousel-item bg-white rounded-box desktop:rounded-none"
+          className="py-1.5 px-6 carousel-item bg-white rounded-box desktop:rounded-none"
           onClick={changeCategory}
         >
-          <button className=" focus:stroke-orange-600 focus:text-orange-600 hover:text-orange-600 hover:stroke-orange-600">
+          <button className="flex flex-col items-center focus:stroke-orange-600 focus:text-orange-600 hover:text-orange-600 hover:stroke-orange-600">
             <Bowl />
             <span>Боулы</span>
           </button>
         </li>
         <li
-          className="flex flex-col items-center py-1.5 px-6 carousel-item  bg-white rounded-box desktop:rounded-none"
+          className=" py-1.5 px-6 carousel-item  bg-white rounded-box desktop:rounded-none"
           onClick={changeCategory}
         >
-          <button className=" focus:stroke-orange-600 focus:text-orange-600 hover:text-orange-600 hover:stroke-orange-600">
+          <button className="flex flex-col items-center focus:stroke-orange-600 focus:text-orange-600 hover:text-orange-600 hover:stroke-orange-600">
             <Drink />
             <span>Напитки</span>
           </button>
         </li>
         <li
-          className="flex flex-col items-center py-1.5 px-6 carousel-item bg-white rounded-box desktop:rounded-none"
+          className="py-1.5 px-6 carousel-item bg-white rounded-box desktop:rounded-none"
           onClick={changeCategory}
         >
-          <button className=" focus:stroke-orange-600 focus:text-orange-600 hover:text-orange-600 hover:stroke-orange-600">
+          <button className="flex flex-col items-center focus:stroke-orange-600 focus:text-orange-600 hover:text-orange-600 hover:stroke-orange-600">
             <Sauce />
             <span>Соусы</span>
           </button>
@@ -78,7 +78,7 @@ const Category = ({ updateCategory }: CategoryProps) => {
           <Unknown />
           <span>Неизв</span>
         </li>
-        <li className="flex flex-col items-center py-1.5 px-6 text-[#9E9E9E] carousel-item  bg-white rounded-box desktop:rounded-none">
+        <li className="flex flex-col items-center py-1.5 px-6 text-[#9E9E9E] carousel-item  bg-white desktop:rounded-none desktop:rounded-r-2xl">
           <Unknown />
           <span>Неизв</span>
         </li>

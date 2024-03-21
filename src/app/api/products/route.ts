0c6c78f42b5a0e1a74152a03d51b0b53cda32f08
sofiialives <127687828+sofiialives@users.noreply.github.com>
2024-03-21 21:54:56 +0100

    pagination + filter

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,15 +1,22 @@
 import { dbConnect } from '@/lib/db/connect';
 import { Product } from '@/lib/models/products';
-import { NextApiRequest } from 'next';
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
-export const GET = async (request: NextApiRequest) => {
+export const GET = async (request: NextRequest) => {
   try {
     dbConnect();
-    const products = await Product.find();    
+    const searchParams = request.nextUrl.searchParams;
+    const query = searchParams.get('category');
+    const page = searchParams.get('page') || 0;
+    const limit = searchParams.get('limit');
+    const skip = (Number(page) - 1) * Number(limit);
+
+    const products = await Product.find({ category: Number(query) }, null, {
+      skip,
+      limit: Number(limit),
+    });
     console.log(products);
     return NextResponse.json(products);
-
   } catch (error) {
     console.error(error);
     throw new Error('Failed to fetch products');
--- src/lib/routes/products.ts
+++ src/lib/routes/products.ts
@@ -1,14 +1,24 @@
-export const getProducts = async () => {
+export const getProducts = async ({
+  category,
+  limit,
+  page,
+}: {
+  category: number;
+  limit?: number;
+  page?: number;
+}) => {
   try {
-    
-    const res = await fetch(`http://localhost:3000/api/products`, {
-      next: { revalidate: 3600 },
-    });
+    const res = await fetch(
+      `http://localhost:3000/api/products?category=${category}&&page=${page}&&limit=${limit}`,
+      {
+        next: { revalidate: 3600 },
+      }
+    );
     if (!res.ok) {
       throw new Error('Something went wrong');
-    }  
+    }
     return res.json();
-  } catch(err) {
+  } catch (err) {
     console.log(err);
   }
 };

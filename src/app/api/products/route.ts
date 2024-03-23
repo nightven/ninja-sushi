c444cf59af0240a637fd3bc583b50e3ec6027bd3
sofiialives <127687828+sofiialives@users.noreply.github.com>
2024-03-24 00:06:03 +0100

    no filters

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -5,34 +5,8 @@ import { NextRequest, NextResponse } from 'next/server';
 export const GET = async (request: NextRequest) => {
   try {
     dbConnect();
-    const searchParams = request.nextUrl.searchParams;
-    const category = searchParams.get('category');
-    const page = searchParams.get('page') || 1;
-    const limit = searchParams.get('limit') || 12;
-    const lactose = searchParams.get('lactose') === 'true';
-    const vegan = searchParams.get('vegan') === 'true';
-    const spicy = searchParams.get('spicy') === 'true';
 
-    const filter: Record<string, any> = { category };
-
-    if (vegan) {
-      filter.vegan = true;
-    }
-    if (lactose) {
-      filter.lactose = true;
-    }
-    if (spicy) {
-      filter.spicy = true;
-    }
-
-    const skip = (Number(page) - 1) * Number(limit);
-
-    const products = await Product.find(filter, null, {
-      skip,
-      limit: Number(limit),
-    });
-
-    console.log(products);
+    const products = await Product.find();
     return NextResponse.json(products);
   } catch (error) {
     console.error(error);

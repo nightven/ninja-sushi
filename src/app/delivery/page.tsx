1d1960c637339adf9136e6fceb845a40850db3b6
sofiialives <127687828+sofiialives@users.noreply.github.com>
2024-03-27 00:32:28 +0100

    panel added

--- src/app/delivery/page.tsx
+++ src/app/delivery/page.tsx
@@ -3,6 +3,7 @@ import About from '@/components/Delivery/about-section/About';
 import Condition from '@/components/Delivery/condition-section/Condition';
 import ZonesList from '@/components/Delivery/first-section/ZonesList';
 import DeliveryList from '@/components/Delivery/second-section/DeliveryList';
+import Panel from '@/components/Panel/Panel';
 import clsx from 'clsx';
 import React from 'react';
 
@@ -11,6 +12,11 @@ interface DeliveryProps {}
 export default function Delivery({}: DeliveryProps) {
   return (
     <main>
+      <section className="container mt-4">
+        <Panel>
+          <p className="text-sm text-gray-400">Оформлення замовлення</p>
+        </Panel>
+      </section>
       <DeliveryContainer className="container">
         <h1 className="desktop:text-5xl text-3xl font-bold desktop:mb-8 mb-3">
           Доставка
